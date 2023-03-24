0d6cce029a6e0e493345e7afd2c2fb6dedf9f9ce
MinhLong2410-02 <minhlong2002@gmail.com>
2023-03-24 22:02:17 +0700

    add model student

--- Vis4T_main/HomePage/urls.py
+++ Vis4T_main/HomePage/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('login/', views.login, name='login'),
+    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('home/', auth_views.LoginView.as_view(template_name='./home/home.html'), name='home'),
 ]
--- Vis4T_main/HomePage/views.py
+++ Vis4T_main/HomePage/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import LoginForm
-from .models import Teacher, University_class
+from .models import Teacher, University_class, Student
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
-@login_required
+@login_required(login_url='login')
+@csrf_protect 
 def home(request, teacher, classes):
-    context = {'teacher': teacher, 'classes': classes}
+    students = Student.objects.filter(class_name=classes[2])
+    print(students)
+    context = {'teacher': teacher, 'classes': classes, 'students': students}
     return render(request, './home/home.html', context=context)
 
         
-def login(request):
+def login_view(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            teacher = Teacher.objects.get(username=username)
-            classes = University_class.objects.filter(teacher=teacher)
-            return home(request, teacher, classes)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                teacher = Teacher.objects.get(username=username)
+                classes = University_class.objects.filter(teacher=teacher)
+                return home(request, teacher, classes)
+            else:
+                messages.error(request, 'Tài khoản hoặc mật khẩu không đúng')
+                redirect('login')
             # return render(request, 'home/home.html', context={'teacher': teacher})
     return render(request, 'login/login_form.html', 
                   {'form': LoginForm})
