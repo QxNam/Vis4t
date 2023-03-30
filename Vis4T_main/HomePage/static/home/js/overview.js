be09f8bb8744419efedb375ff774031c55298c5f
MinhLong2410-02 <minhlong2002@gmail.com>
2023-03-30 13:29:41 +0700

    update pie chart color

--- Vis4T_main/HomePage/static/home/js/overview.js
+++ Vis4T_main/HomePage/static/home/js/overview.js
@@ -1,62 +1,69 @@
 function renderCSVTable(class_name){
-    fetch(get_class_url(class_name))
-    .then(response => response.json())
-    .then(data => {
-        var tabledata = []
-        var { class_info, student, score_char_data, rank_data } = data.data;
-        for (var i = 0; i < student.length; i++) {
-            tabledata.push({
-                "Mã số": student[i].student_id,
-                "Họ tên": student[i].student_name,
-                "Điểm 10": student[i].score_10,
-                "Điểm 4": student[i].score_4,
-                "Điểm chữ": student[i].score_char,
-                "Xếp hạng": student[i].rank,
-                "Số tín chỉ đã học xong": student[i].passed_credit
-            })
-        }
-        new Tabulator(".csv-table", {
-            placeholder:"Chưa có lớp",
-            data: tabledata,
-            layout:"fitColumns",
+  fetch(get_class_url(class_name))
+  .then(response => response.json())
+  .then(data => {
+      var tabledata = []
+      var { class_info, student, score_char_data, rank_data } = data.data;
+      for (var i = 0; i < student.length; i++) {
+          tabledata.push({
+              "Mã số": student[i].student_id,
+              "Họ tên": student[i].student_name,
+              "Điểm 10": student[i].score_10,
+              "Điểm 4": student[i].score_4,
+              "Điểm chữ": student[i].score_char,
+              "Xếp hạng": student[i].rank,
+              "Số tín chỉ đã học xong": student[i].passed_credit
+          })
+      }
+      new Tabulator(".csv-table", {
+          placeholder:"Chưa có lớp",
+          data: tabledata,
+          layout:"fitColumns",
 
-            tooltips:true,  
-            columns: [
-                {title: "Mã số", field: "Mã số", width: 80},
-                {title: "Họ tên", field: "Họ tên", width: 155},
-                {title: "Điểm 10", field: "Điểm 10", sorter:"number", align:"right", width: 90},
-                {title: "Điểm 4", field: "Điểm 4", width: 90},
-                {title: "Điểm chữ", field: "Điểm chữ", width: 100},
-                {title: "Xếp hạng", field: "Xếp hạng", width: 120},
-                {title: "Số tín chỉ", field: "Số tín chỉ đã học xong", width: 100},
-            ]  
-        });
+          tooltips:true,  
+          columns: [
+              {title: "Mã số", field: "Mã số", width: 80},
+              {title: "Họ tên", field: "Họ tên", width: 155},
+              {title: "Điểm 10", field: "Điểm 10", sorter:"number", align:"right", width: 90},
+              {title: "Điểm 4", field: "Điểm 4", width: 90},
+              {title: "Điểm chữ", field: "Điểm chữ", width: 100},
+              {title: "Xếp hạng", field: "Xếp hạng", width: 120},
+              {title: "Số tín chỉ", field: "Số tín chỉ đã học xong", width: 100},
+          ]  
+      });
 
         const ctx = document.getElementById('myClassChart');
+        const LABELS = {'A+': '#257a24', 'A': '#3C8321', 'B': '#5A8C1E', 
+                        'B+': '#7E951A', 'C': '#9E9315', 'C+': '#A38413', 
+                        'D': '#AC600D', 'D+': '#B53307', 'F': '#BF0303'}; // Array of labels
+        
+
+        // Sort the data by value in descending order
+        
+        // Create the chart with the modified data and colors
         var chart = new Chart(ctx, {
           type: 'pie',
           data: {
-            labels: score_char_data[0],
+            labels: Object.keys(score_char_data),
             datasets: [{
-              data: score_char_data[1],
-              borderWidth: 1
+              data: Object.values(score_char_data),
+              borderWidth: 1,
+              backgroundColor: Object.values(LABELS)
             }]
           },
           options: {
             plugins: {
               legend: {
                 display: true,
-                position: 'pie'
-              }
-            },
-            scales: {
-              y: {
-                beginAtZero: true
+                position: 'top',
+                labels: {
+                  color: 'green'
+                }
               }
             }
           }
         });
-    });
+  });
 }
 
 $(".dropdown-class").on("click", function() {
