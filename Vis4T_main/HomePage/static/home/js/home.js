4e2493d787316dc4d8ef5de884ae43a65aa01886
MinhLong2410-02 <minhlong2002@gmail.com>
2023-03-30 12:16:42 +0700

    update color for option

--- Vis4T_main/HomePage/static/home/js/home.js
+++ Vis4T_main/HomePage/static/home/js/home.js
@@ -1,8 +1,11 @@
+
 $(".dropdown-item").on("click", function() {
     var [name_class, number] = this.id.split(" ");
     $("#class-name").html(name_class);
     $("#current-class-number").html(number);
     $("#dropdownMenuButton").html(name_class);
+
+    
 });
 
 var chartPos = $(".chart").offset().left;
