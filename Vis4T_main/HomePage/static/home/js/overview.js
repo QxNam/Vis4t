1925477d5f8140d8bca18f14be89e38a3943b872
MinhLong2410-02 <minhlong2002@gmail.com>
2023-04-01 18:32:33 +0700

    add button in pie chart

--- Vis4T_main/HomePage/static/home/js/overview.js
+++ Vis4T_main/HomePage/static/home/js/overview.js
@@ -1,3 +1,51 @@
+function is_checked() {
+  return $('#flexSwitchCheckDefault').is(':checked');
+}
+function render_pie_chart(class_name, score_text = 'ĐIỂM CHỮ') {
+  fetch(get_class_url(class_name))
+  .then(response => response.json())
+  .then(data => {
+    const ctx = document.getElementById('myClassChart');
+    const LABELS = {'A+': '#257a24', 'A': '#3C8321', 'B': '#5A8C1E', 
+                    'B+': '#7E951A', 'C': '#9E9315', 'C+': '#A38413', 
+                    'D': '#AC600D', 'D+': '#B53307', 'F': '#BF0303'}; // Array of labels
+    var { class_info, student, score_char_data, rank_data } = data.data;
+    if (score_text == 'HỌC LỰC') {
+      rendered_data = rank_data;
+    }
+    else {
+      rendered_data = score_char_data;
+    }
+    var chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: Object.keys(rendered_data),
+        datasets: [{
+          data: Object.values(rendered_data),
+          borderWidth: 1,
+          backgroundColor: Object.values(LABELS)
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'top',
+            labels: {
+              color: 'green'  
+            }
+          },
+          title: {
+            display: true,
+            text: `BIỂU ĐỒ THỐNG KÊ ${score_text} CỦA LỚP ` + class_name,
+            fontSize: 30
+          }
+        }
+      }
+    });
+  })
+}
+
 function renderCSVTable(class_name){
   fetch(get_class_url(class_name))
   .then(response => response.json())
@@ -27,43 +75,12 @@ function renderCSVTable(class_name){
               {title: "Điểm 10", field: "Điểm 10", sorter:"number", align:"right", width: 90},
               {title: "Điểm 4", field: "Điểm 4", width: 90},
               {title: "Điểm chữ", field: "Điểm chữ", width: 100},
-              {title: "Xếp hạng", field: "Xếp hạng", width: 120},
+              {title: "Học lực", field: "Xếp hạng", width: 120},
               {title: "Số tín chỉ", field: "Số tín chỉ đã học xong", width: 100},
           ]  
       });
 
-      const ctx = document.getElementById('myClassChart');
-      const LABELS = {'A+': '#257a24', 'A': '#3C8321', 'B': '#5A8C1E', 
-                      'B+': '#7E951A', 'C': '#9E9315', 'C+': '#A38413', 
-                      'D': '#AC600D', 'D+': '#B53307', 'F': '#BF0303'}; // Array of labels
-      console.log(score_char_data);
-      var chart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: Object.keys(score_char_data),
-          datasets: [{
-            data: Object.values(score_char_data),
-            borderWidth: 1,
-            backgroundColor: Object.values(LABELS)
-          }]
-        },
-        options: {
-          plugins: {
-            legend: {
-              display: true,
-              position: 'top',
-              labels: {
-                color: 'green'  
-              }
-            },
-            title: {
-              display: true,
-              text: 'BIỂU ĐỒ THỐNG KÊ ĐIỂM CHỮ CỦA LỚP ' + class_name,
-              fontSize: 30
-            }
-          }
-        }
-      });
+      
   });
 }
 
@@ -72,10 +89,34 @@ $(".dropdown-class").on("click", function() {
     $("canvas").remove();
     $(".pie-chart").append('<canvas id="myClassChart" width="400" height="400"></canvas>');
     renderCSVTable(class_name);
+    is_check = is_checked();
+    if (!is_check) {
+      render_pie_chart(class_name);
+    }
+    else {
+      render_pie_chart(class_name, 'HỌC LỰC');
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     var class_name = document.getElementById("class-name").innerHTML;
     renderCSVTable(class_name);
+    render_pie_chart(class_name)
 });
 
+let isCheckChangeChart = $('.btn-change-chart')
+  isCheckChangeChart.click(function(){
+      if(isCheckChangeChart.is(":checked")){
+          $('.text_score').toggleClass('actBtn');
+          $('.academic-rank').toggleClass('actBtn');
+          score_text = "HỌC LỰC";
+      }else{
+          $('.text_score').toggleClass('actBtn');
+          $('.academic-rank').toggleClass('actBtn');
+          score_text = "ĐIỂM CHỮ";
+      }
+      $("canvas").remove();
+      $(".pie-chart").append('<canvas id="myClassChart" width="400" height="400"></canvas>');
+      var class_name = $('#class-name').text();
+      render_pie_chart(class_name, score_text);
+  })
