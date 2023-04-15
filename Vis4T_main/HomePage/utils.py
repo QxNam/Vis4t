d38910e0648ec61e276f7c6fccef6c3c03e1b46a
MinhLong2410-02 <minhlong2002@gmail.com>
2023-04-15 10:45:42 +0700

    update utils.py

--- Vis4T_main/HomePage/utils.py
+++ Vis4T_main/HomePage/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def query_student_data_with_nominal_data(student_data, column_name):
+def query_student_data_with_ordinal_data(student_data, column_name):
     df = pd.DataFrame(student_data)
     new_df = df.groupby(column_name).aggregate({'student_id': 'count'}).rename(columns={'student_id': 'count'})
     return new_df.to_dict()
