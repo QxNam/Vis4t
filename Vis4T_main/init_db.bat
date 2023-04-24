0f2b4035bebdb82972d33b9075109399404e0f58
MinhLong2410-02 <minhlong2002@gmail.com>
2023-04-25 01:16:25 +0700

    upgrade init_db script

--- Vis4T_main/init_db.bat
+++ Vis4T_main/init_db.bat
@@ -1,5 +1,9 @@
 @echo off
-
+del db.sqlite3
+del HomePage\migrations\0001_initial.py
+rmdir /s /q HomePage\migrations\__pycache__
+rmdir /s /q HomePage\__pycache__
+rmdir /s /q Vis4T_main\__pycache__
 call migrate.bat
 call python init_db.py
 @REM call createsuperuser.bat
