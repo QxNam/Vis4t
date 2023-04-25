bb5ea7511e7b0f0788862908ea8e96e68434331f
MinhLong2410-02 <minhlong2002@gmail.com>
2023-04-26 01:18:52 +0700

    using ajax requests

--- Vis4T_main/HomePage/static/home/js/overview.js
+++ Vis4T_main/HomePage/static/home/js/overview.js
@@ -1,10 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    renderCSVTable(class_name);
-    fetch(get_class_url(class_name))
-    .then(response => response.json())
-    .then(data => {
+$(document).ready(function() {
+  renderCSVTable(class_name);
+  $.ajax({
+    url: get_class_url(class_name),
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
       render_pie_chart(data);
       render_bar_chart(data, class_name);
       render_box_plot(data, class_name);
-    });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log('Error:', errorThrown);
+    }
+  });
 });
