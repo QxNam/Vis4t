38b99d4e4ce4ab8f219b467e499bb2c2ab3730db
MinhLong2410-02 <minhlong2002@gmail.com>
2023-05-04 20:15:22 +0700

    change required field in Teacher Model to teacher_id for login

--- Vis4T_main/init_db.bat
+++ Vis4T_main/init_db.bat
@@ -4,4 +4,8 @@ del HomePage\migrations\0001_initial.py
 
 call migrate.bat
 call python init_db.py
+
+rmdir /s /q HomePage\migrations\__pycache__
+rmdir /s /q HomePage\__pycache__
+rmdir /s /q Vis4T_main\__pycache__
 @REM call createsuperuser.bat
