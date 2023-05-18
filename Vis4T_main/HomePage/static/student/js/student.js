aa4f2d58e97b9f727b04e331308b45aadb377328
MinhLong2410-02 <minhlong2002@gmail.com>
2023-05-18 11:30:48 +0700

    add histogram in student view

--- Vis4T_main/HomePage/static/student/js/student.js
+++ Vis4T_main/HomePage/static/student/js/student.js
@@ -129,15 +129,93 @@ function renderStudentDetailBarChart(data) {
           );
           chart.render();
 }
+
+function renderHistChartOnScoreData(data) {
+  var subject_score = data;
+  const chartData = subject_score.map(entry => ({
+      x: entry["subject__subject_name"],
+      y: entry.score_10,
+  }));
+  var rangeFreq = {
+      "0-5":0,
+      "5-6":0,
+      "6-7":0,
+      "7-8":0,
+      "8-9":0,
+      "9-10":0
+  };
+  scores = chartData.map(entry => entry.y);
+  for (var y in scores) {
+    score = scores[y];
+    if (score <= 5) {
+        rangeFreq["0-5"]++;
+    } else if(score <= 6) {
+        rangeFreq["5-6"]++;
+    } else if(score <= 7) {
+        rangeFreq["6-7"]++;
+    } else if(score <= 8) {
+        rangeFreq["7-8"]++;
+    } else if(score <= 9) {
+        rangeFreq["8-9"]++;
+    } else {
+        rangeFreq["9-10"]++;
+    } 
+  }
+
+  var options = {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Phổ điểm trung bình môn học'
+    },
+    xAxis: {
+        categories: Object.keys(rangeFreq),
+        min: 0,
+        title: {
+            text: 'Phổ điểm'
+        }
+    },
+    yAxis: {
+        crosshair: true,
+        title: {
+            text: 'Số lượng môn'
+        }
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0,
+            borderWidth: 0,
+            groupPadding: 0,
+            shadow: false
+        },
+        spline: {
+            lineWidth: 2,
+            marker: {
+                enabled: false
+            }
+        }
+    },
+    series: [{
+        name: 'Số lượng môn',
+        binwidth: 4,
+        data: Object.values(rangeFreq)
+    }]
+  };
+  chart = Highcharts.chart('myHistChart', options);
+}
+
+
 drawCreditRadialChart();
 renderStudentDetailBarChart(subject_list);
+renderHistChartOnScoreData(subject_list);
 
 $('.dropdown-class').click(function(){
   var id = $(this).attr('data-student-id');
   window.location.href = '/student/' + id;
 });
 
-var table2 = new Tabulator("#table-student-object-score", {
+var table = new Tabulator("#table-student-object-score", {
   data: subject_list,
   layout:"fitColumns",
   tooltips:true, 
