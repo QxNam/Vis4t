88cba2d76767e868651f981be918c2939a2310a0
MinhLong2410-02 <minhlong2002@gmail.com>
2023-05-29 20:30:13 +0700

    update settings.py

--- Vis4T_main/Vis4T_main/settings.py
+++ Vis4T_main/Vis4T_main/settings.py
@@ -37,6 +37,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS = [
     'rest_framework',
     'HomePage',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 STATIC_URL = '/static/' 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
